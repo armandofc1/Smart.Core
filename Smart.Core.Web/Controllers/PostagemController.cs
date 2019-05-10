879fd67db8d7e9ec6492bfb7b1dd039c7cacfd8a
armandofc1 <armandofc1@gmail.com>
2019-05-10 18:15:17 -0300

    binding
    binding

---
+++ Smart.Core.Web/Controllers/PostagemController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Smart.Core.Domain.Entities;
+using Smart.Core.Infra.Context;
+using Smart.Core.Web.Models;
+using System.Diagnostics;
+
+namespace Smart.Core.Web.Controllers
+{
+    public class PostagemController : Controller
+    {
+        private readonly SmartContext _context;
+
+        public PostagemController(SmartContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? codigo)
+        {
+            if (codigo == null)
+            {
+                return NotFound();
+            }
+
+            var postagem = await _context.Postagem
+                .FirstOrDefaultAsync(m => m.Codigo == codigo);
+            if (postagem == null)
+            {
+                return NotFound();
+            }
+
+            return View(postagem);
+        }
+    }
+}
