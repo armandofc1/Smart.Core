177514e5ce9ef584fcb09fec800d86460c00b442
armandofc1 <armandofc1@gmail.com>
2019-05-13 16:12:52 -0300

    components
    components

---
+++ Smart.Core.Web/Components/Categorias.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Smart.Core.Domain.Entities;
+using Smart.Core.Infra.Context;
+using Smart.Core.Web.Models;
+using System.Diagnostics;
+
+
+namespace Smart.Core.Web.Components
+{
+    public class Categorias : ViewComponent
+    {
+        private readonly SmartContext _context;
+
+        public Categorias(SmartContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _context.Categoria.ToListAsync();
+            return View(items);
+        }
+    }
+}
---
+++ Smart.Core.Web/Components/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Smart.Core.Domain.Entities;
+using Smart.Core.Infra.Context;
+using Smart.Core.Web.Models;
+using System.Diagnostics;
+
+namespace Smart.Core.Web.Components
+{
+    public class Menu : ViewComponent
+    {
+        private readonly SmartContext _context;
+
+        public Menu(SmartContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
